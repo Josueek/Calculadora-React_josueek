d30abc03841693e2fc6f41dcbb1336af5c9e8488
Josueek <abdelmario351@gmail.com>
2024-05-03 11:00:49 -0600

    Proyecto de calculadora

---
+++ src/components/Botones.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+
+export default function Botones() {
+
+
+}
